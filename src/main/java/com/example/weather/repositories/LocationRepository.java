3b450023da1147ef9e08375bd6a47e2d0a30de88
Timur Khusnutdinov <t.m.khusnutdinov@gmail.com>
2024-03-17 20:30:30 +0200

    Adding locationService

--- src/main/java/com/example/weather/repositories/LocationRepository.java
+++ src/main/java/com/example/weather/repositories/LocationRepository.java
@@ -3,5 +3,8 @@ package com.example.weather.repositories;
 import com.example.weather.models.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
+    Optional<Location> findByName(String name);
 }
---
+++ src/main/java/com/example/weather/services/LocationService.java
@@ -0,0 +1,43 @@
+package com.example.weather.services;
+
+import com.example.weather.models.Location;
+import com.example.weather.repositories.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+class LocationService {
+    private final LocationRepository locationRepository;
+
+    @Autowired
+    LocationService(LocationRepository locationRepository) {
+        this.locationRepository = locationRepository;
+    }
+
+    public List<Location> getAllLocations() {
+        return locationRepository.findAll();
+    }
+
+    public Optional<Location> getLocationByName(String name) {
+        return locationRepository.findByName(name);
+    }
+
+    public Optional<Location> getLocationById(Long id) {
+        return locationRepository.findById(id);
+    }
+
+    public Location saveLocation(Location location) {
+        return locationRepository.save(location);
+    }
+
+    public String deleteLocation(Long id) {
+        Location locationToDelete = locationRepository.findById(id).orElseThrow();
+        String deleteMessage = String.format("Location with id %d and name %s was successfully deleted",
+                id, locationToDelete.getName());
+        locationRepository.deleteById(id);
+        return deleteMessage;
+    }
+}
